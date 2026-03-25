7dd3cc3a06d84ff5672a4a89c3cfdb30f503f8a8
DávidGergő <david1gergo726@gmail.com>
2026-03-25 09:18:27 +0100

    done

---
+++ gyak6/Program.cs
@@ -0,0 +1,94 @@
+﻿using AlkalmazottNevter;
+
+namespace FuttathatóNevter
+{
+    internal class Futtathato
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NYugdíjazas");
+
+            Alkalmazott alkalmazott = new Alkalmazott();
+
+            int evek_nyugdijig = alkalmazott.EvekNyugdijig();
+            Console.WriteLine("Évek nyugdíjig: " + evek_nyugdijig + "\n\n");
+
+            int alkalmazottak_szama = 5;
+
+            Alkalmazott[] alkalmazottak = new Alkalmazott[alkalmazottak_szama];
+
+            Random rnd = new Random();
+            Console.WriteLine("Új alkalmazottak:");
+            for (int i = 0; i < alkalmazottak.Length; i++)
+            {
+                alkalmazottak[i] = new Alkalmazott("XY" + (i + 1), rnd.Next(25, Alkalmazott.Nyugdij_korhatar));
+                Console.WriteLine("\t" + alkalmazottak[i]);
+            }
+
+            Alkalmazott.SetNyugdijkorhatar(70);
+            Alkalmazott.Nyugdij_korhatar = 70;
+
+            Console.WriteLine("\nÚj nyugdíjkorhatárral: ");
+            AlkalmazottKiir(alkalmazottak);
+
+            Console.WriteLine("\nAkiknek < 5 -nél kevesebb idő nyugdíjig.");
+            otevnelkevesebbnyugdijig(alkalmazottak);
+
+            RendezesNYEvekAlapjan(alkalmazottak);
+
+        }
+
+        private static void RendezesNYEvekAlapjan(Alkalmazott[] alkalmazottak)
+        {
+            for (int i = 0; i < alkalmazottak.Length - 1; i++)
+            {
+                int minIND = i;
+                for (int j = i + 1; j < alkalmazottak.Length; j++)
+                {
+                    if (alkalmazottak[j].EvekNyugdijig() < alkalmazottak[minIND].EvekNyugdijig())
+                    {
+                        minIND = j;
+                    }
+                }
+
+                if (minIND != i)
+                {
+                    Alkalmazott temp = alkalmazottak[i];
+                    alkalmazottak[i] = alkalmazottak[minIND];
+                    alkalmazottak[minIND] = temp;
+                }
+            }
+
+            Console.WriteLine("\nRendezett alkalmazottak (évek nyugdíjig növekvő):");
+            AlkalmazottKiir(alkalmazottak);
+        }
+
+        private static void otevnelkevesebbnyugdijig(Alkalmazott[] alkalmazottak)
+        {
+            int c = 0;
+            for (int i = 0; i < alkalmazottak.Length; i++)
+            {
+                if (alkalmazottak[i].EvekNyugdijig() < 15)
+                {
+                    c++;
+                    Console.WriteLine("\t" + alkalmazottak[i]);
+                }
+            }
+
+            if (c == 0)
+            {
+                Console.WriteLine("\tNincs olyan akinek ilyen kevés ideje van nyugdijig.");
+            }
+        }
+
+        private static void AlkalmazottKiir(Alkalmazott[] alkalmazottak)
+        {
+            for (int i = 0; i < alkalmazottak.Length; i++)
+            {
+                Console.WriteLine("\t" + alkalmazottak[i]);
+            }
+        }
+    
+        
+    }
+}
--- gyak6/alkalmazott.cs
+++ gyak6/alkalmazott.cs
@@ -6,7 +6,79 @@ using System.Threading.Tasks;
 
 namespace AlkalmazottNevter
 {
-    internal class alkalmazott
+    internal class Alkalmazott
     {
+		private string nev;
+		private int kor;
+		private long fizetes;
+		private static int nyugdijkorhatar = 65;
+
+        public Alkalmazott(string nev, int kor, long fizetes)
+        {
+            this.nev = nev;
+            this.kor = kor;
+            this.fizetes = fizetes;
+        }
+        public Alkalmazott(string nev, int kor) : this(nev, kor, 10000 * kor)
+        {
+
+        }
+        public Alkalmazott() : this("nevtelen", 25) 
+		{
+		
+		}
+
+        public static int Nyugdij_korhatar
+		{
+			get { return nyugdijkorhatar; }
+			set { nyugdijkorhatar = value; }
+		}
+
+		public static void SetNyugdijkorhatar(int value)
+		{
+			nyugdijkorhatar = value;
+		}
+
+		public long Fizetes
+		{
+			get { return fizetes; }
+			set { fizetes = value; }
+		}
+
+		public int Kor
+		{
+			get { return kor; }
+			set { kor = value; }
+		}
+
+		public string Nev
+		{
+			get { return nev; }
+			set { nev = value; }
+		}
+
+        internal int EvekNyugdijig()
+        {
+			return Nyugdij_korhatar - kor;
+        }
+
+		public static Alkalmazott TobbEveVanNyugdijig(Alkalmazott egyik, Alkalmazott masik)
+		{
+			if (egyik.EvekNyugdijig() > masik.EvekNyugdijig())
+			{
+				return egyik;
+			}
+
+			return masik;
+        }
+        
+		public override string ToString()
+        {
+			return "nev: " + Nev + ", kor: " + Kor + ", Fizetés: " + Fizetes + ", Évek Nyugdíjig: " + EvekNyugdijig();
+        }
+
+
+
+
     }
 }
